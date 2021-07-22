608226afda82b072e22803c5c3fd695d0e9eef13
Yasmin <yasmin.s.luzia@gmail.com>
2021-07-22 13:55:41 -0300

    Part I - Beginning the project
    App bar, icon, cake image and first column.

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tudo_gostoso/screens/home/widgets/details.dart';
+import 'package:tudo_gostoso/style.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: orangeTheme,
+        title: Text("Tudo Gostoso",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),),
+        leading: Image.asset("assets/images/logo.jpg"),
+      ),
+      body: Column(
+        children: [
+          Image.asset("assets/images/bolo.jpg"),
+          Details(),
+        ],
+      ),
+    );
+  }
+}
